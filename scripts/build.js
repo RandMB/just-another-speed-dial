e723f5e382eebd7db0391ebc1cbd4a57384e1f7c
Mantas <mantasb74@gmail.com>
2018-01-11 02:01:32 +0200

    Removing config sidebar
    - Cofiguration will be provided through a dedicated extension page

--- scripts/build.js
+++ scripts/build.js
@@ -11,13 +11,11 @@ process.on('unhandledRejection', err => {
     throw err;
 });
 
-const path = require('path');
 const chalk = require('chalk');
 const fs = require('fs-extra');
 const webpack = require('webpack');
 const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
 const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
-const printHostingInstructions = require('react-dev-utils/printHostingInstructions');
 const FileSizeReporter = require('react-dev-utils/FileSizeReporter');
 const printBuildError = require('react-dev-utils/printBuildError');
 
@@ -35,13 +33,11 @@ const printFileSizesAfterBuild = FileSizeReporter.printFileSizesAfterBuild;
 const WARN_AFTER_BUNDLE_GZIP_SIZE = 512 * 1024;
 const WARN_AFTER_CHUNK_GZIP_SIZE = 1024 * 1024;
 
-function buildWithConfig(paths, config) {
+function buildWithConfig(paths, configs) {
     // Warn and crash if required files are missing
     if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
         process.exit(1);
     }
-
-    const useYarn = fs.existsSync(paths.yarnLockFile);
 
     // First, read the current file sizes in build directory.
     // This lets us display how much they changed later.
@@ -82,18 +78,6 @@ function buildWithConfig(paths, config) {
             );
             console.log();
 
-            const appPackage = require(paths.appPackageJson);
-            const publicUrl = paths.publicUrl;
-            const publicPath = config.output.publicPath;
-            const buildFolder = path.relative(process.cwd(), paths.appBuild);
-            printHostingInstructions(
-                appPackage,
-                publicUrl,
-                publicPath,
-                buildFolder,
-                useYarn
-            );
-
             return Promise.resolve();
         },
         err => {
@@ -107,7 +91,7 @@ function buildWithConfig(paths, config) {
     function build(previousFileSizes) {
         console.log('Creating an optimized production build...');
 
-        let compiler = webpack(config);
+        let compiler = webpack(configs);
         return new Promise((resolve, reject) => {
             compiler.run((err, stats) => {
                 if (err) {
@@ -146,6 +130,10 @@ function buildWithConfig(paths, config) {
     }
 }
 
+console.time('build');
+
 buildWithConfig(dialAppPaths, dialAppConfig).then(() =>{
-    buildWithConfig(optionsAppPaths, optionsAppConfig).then();
+    buildWithConfig(optionsAppPaths, optionsAppConfig).then(() => {
+        console.timeEnd('build');
+    });
 });
--- speed-dial-app/src/components/app/App.jsx
+++ speed-dial-app/src/components/app/App.jsx
@@ -3,11 +3,8 @@ import PropTypes from 'prop-types';
 
 import SpeedDialContainer from '../speed-dial-container/SpeedDialContainer';
 import FolderPickerContainer from '../folder-picker-container/FolderPickerContainer';
-import ConfigurationContainer from '../configuration-container/ConfigurationContainer';
 
 import _isEmpty from 'lodash/isEmpty';
-import _merge from 'lodash/merge';
-import _cloneDeep from 'lodash/merge';
 
 import './App.css';
 import backgroundImageUrl from '../../assets/background-default.jpg';
@@ -39,13 +36,11 @@ class App extends Component {
             config: {
                 rootId: null,
             },
+
             isLoaded: false,
-            isConfigLoaded: true,
-            isConfigOpen: false,
         };
 
         this.onFolderSelect = this.onFolderSelect.bind(this);
-        this.onConfigChange = this.onConfigChange.bind(this);
 
         this.configPromise = browser.storage.local.get('config');
     }
@@ -65,14 +60,6 @@ class App extends Component {
                 isLoaded: true,
             });
         }, onError);
-
-        document.getElementById('root').addEventListener('click', (event) => {
-            if (event.target.matches('.config-close') && this.state.isConfigOpen) {
-                this.setState({
-                    isConfigOpen: false,
-                });
-            }
-        }, false);
     }
 
     onFolderSelect(folderId) {
@@ -86,28 +73,6 @@ class App extends Component {
 
         this.setState({
             config: newConfig,
-        });
-    }
-
-    onConfigChange(newPartialConfig) {
-        const newConfig = _cloneDeep(this.state.config);
-        _merge(newConfig, newPartialConfig);
-
-        this.setState({
-            config: newConfig,
-        });
-
-        browser.storage.local.set({ config: newConfig }).then(null, onError);
-    }
-
-    setConfigSidebarOpen(isOpen, event) {
-        if (event) {
-            event.stopPropagation();
-        }
-
-        this.setState({
-            isConfigOpen: isOpen,
-            isConfigLoaded: true,
         });
     }
 
@@ -125,15 +90,6 @@ class App extends Component {
                         onFolderSelect={this.onFolderSelect}
                         rootBookmarkTreeId={rootBookmarkTreeId}>
                     </Container>
-                }
-
-                {this.state.isConfigLoaded &&
-                    <ConfigurationContainer
-                        onClose={() => this.setConfigSidebarOpen(false)}
-                        isOpen={this.state.isConfigOpen}
-                        onConfigChange={this.onConfigChange}
-                        currentConfig={this.state.config}>
-                    </ConfigurationContainer>
                 }
 
                 <div
